9751d1c3776ceb830f4455bc9b200774157df0d7
yohsii <yohsii@hotmail.co.uk>
2019-10-13 19:35:53 +0100

    removed port when port is 80

--- core/Tasks/KeepAliveTask.cs
+++ core/Tasks/KeepAliveTask.cs
@@ -25,7 +25,7 @@ namespace puck.core.Tasks
             await base.Run(t);
             if (PuckCache.FirstRequestUrl != null) {
                 var uri = PuckCache.FirstRequestUrl;
-                var url = $"{uri.Scheme}://{uri.Host}:{uri.Port}/puck/api/keepalive";
+                var url = $"{uri.Scheme}://{uri.Host}{(uri.Port!=80?":"+uri.Port:"")}/puck/api/keepalive";
                 WebRequest request = WebRequest.Create(url);
                 WebResponse response = request.GetResponse();
                 var content = "";
