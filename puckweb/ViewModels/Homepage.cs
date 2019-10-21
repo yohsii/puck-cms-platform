64288ed3d5cc5fa5a7a4f7828ff7d0d54e3d75d4
yohsii <yohsii@hotmail.co.uk>
2019-10-21 19:15:51 +0100

    columns tweak

--- puckweb/ViewModels/Homepage.cs
+++ puckweb/ViewModels/Homepage.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using puck.core.Constants;
 using puck.core.Models;
+using puck.core.Models.EditorSettings.Attributes;
 //using puck.ViewModels;
 
 namespace puckweb.ViewModels
@@ -12,7 +13,9 @@ namespace puckweb.ViewModels
     public class Homepage:Page
     {
         [Display(Name="Carousel Items",GroupName ="Content")]
+        [PuckPickerEditorSettings(MaxPick =5)]
         [UIHint(EditorTemplates.PuckPicker)]
+        
         public List<PuckPicker> CarouselItems { get; set; }
     }
 }
