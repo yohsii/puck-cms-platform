22ac31c5b812734709af8eace1025132824942be
yohsii <yohsii@hotmail.co.uk>
2019-10-24 19:45:02 +0100

    re-added tests project

---
+++ tests/Helpers/MockHelpers.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using puck.core.Abstract;
+using puck.core.Entities;
+
+namespace puck.tests.Helpers
+{
+    public static class MockHelpers
+    {
+        public static StringBuilder LogMessage = new StringBuilder();
+        public static Mock<IServiceProvider> MockServiceProvider(I_Puck_Repository repo) {
+            var serviceProvider = new Mock<IServiceProvider>();
+            var serviceScope = new Mock<IServiceScope>();
+            serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProvider.Object);
+
+            var serviceScopeFactory = new Mock<IServiceScopeFactory>();
+            serviceScopeFactory
+                .Setup(x => x.CreateScope())
+                .Returns(serviceScope.Object);
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IServiceScopeFactory)))
+                .Returns(serviceScopeFactory.Object);
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(I_Puck_Repository)))
+                .Returns(repo);
+
+            return serviceProvider;
+        }
+        public static Mock<UserManager<PuckUser>> MockUserManager()
+        {
+            var store = new Mock<IUserStore<PuckUser>>();
+            var user = new PuckUser() { UserName = "darkezmo@hotmail.com" };
+            var mgr = new Mock<UserManager<PuckUser>>(store.Object, null, null, null, null, null, null, null, null);
+            mgr.Object.UserValidators.Add(new UserValidator<PuckUser>());
+            mgr.Object.PasswordValidators.Add(new PasswordValidator<PuckUser>());
+            mgr.Setup(x => x.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(user));
+            return mgr;
+        }
+
+        public static Mock<RoleManager<TRole>> MockRoleManager<TRole>(IRoleStore<TRole> store = null) where TRole : class
+        {
+            store = store ?? new Mock<IRoleStore<TRole>>().Object;
+            var roles = new List<IRoleValidator<TRole>>();
+            roles.Add(new RoleValidator<TRole>());
+            return new Mock<RoleManager<TRole>>(store, roles, new UpperInvariantLookupNormalizer(),
+                new IdentityErrorDescriber(), null);
+        }
+
+        public static UserManager<TUser> TestUserManager<TUser>(IUserStore<TUser> store = null) where TUser : class
+        {
+            store = store ?? new Mock<IUserStore<TUser>>().Object;
+            var options = new Mock<IOptions<IdentityOptions>>();
+            var idOptions = new IdentityOptions();
+            idOptions.Lockout.AllowedForNewUsers = false;
+            options.Setup(o => o.Value).Returns(idOptions);
+            var userValidators = new List<IUserValidator<TUser>>();
+            var validator = new Mock<IUserValidator<TUser>>();
+            userValidators.Add(validator.Object);
+            var pwdValidators = new List<PasswordValidator<TUser>>();
+            pwdValidators.Add(new PasswordValidator<TUser>());
+            var userManager = new UserManager<TUser>(store, options.Object, new PasswordHasher<TUser>(),
+                userValidators, pwdValidators, new UpperInvariantLookupNormalizer(),
+                new IdentityErrorDescriber(), null,
+                new Mock<ILogger<UserManager<TUser>>>().Object);
+            validator.Setup(v => v.ValidateAsync(userManager, It.IsAny<TUser>()))
+                .Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
+            return userManager;
+        }
+
+        public static RoleManager<TRole> TestRoleManager<TRole>(IRoleStore<TRole> store = null) where TRole : class
+        {
+            store = store ?? new Mock<IRoleStore<TRole>>().Object;
+            var roles = new List<IRoleValidator<TRole>>();
+            roles.Add(new RoleValidator<TRole>());
+            return new RoleManager<TRole>(store, roles,
+                new UpperInvariantLookupNormalizer(),
+                new IdentityErrorDescriber(),
+                null);
+        }
+
+    }
+}
---
+++ tests/ViewModels/Folder.cs
@@ -0,0 +1,12 @@
+﻿using puck.core.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace puck.tests.ViewModels
+{
+    public class Folder:BaseModel
+    {
+
+    }
+}
