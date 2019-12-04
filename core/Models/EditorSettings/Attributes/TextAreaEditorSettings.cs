4befb408d5219785c9d4c4d4f7bea936c8e3572c
yohsii <yohsii@hotmail.co.uk>
2019-12-04 20:02:06 +0000

    Editor settings for TextArea.cshtml and rte.cshtml. allows you to specify width and height.

---
+++ core/Models/EditorSettings/Attributes/TextAreaEditorSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace puck.core.Models.EditorSettings.Attributes
+{
+    [Display(Name ="Text Area Settings")]
+    public class TextAreaEditorSettings : Attribute
+    {
+        public string Width { get; set; }
+        public string Height { get; set; }
+    }
+}
---
+++ core/Models/EditorSettings/Attributes/TinyMCEEditorSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace puck.core.Models.EditorSettings.Attributes
+{
+    [Display(Name ="Rich Text Area Editor Settings")]
+    public class TinyMCEEditorSettings : TextAreaEditorSettings
+    {
+        
+    }
+}
