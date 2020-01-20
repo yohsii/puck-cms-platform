63928e4c67de8aff394fe9b982acf3aad828d3d3
yohsii <yohsii@hotmail.co.uk>
2020-01-20 13:15:46 +0000

    added model properties and comments

--- puckweb/ViewModels/Homepage.cs
+++ puckweb/ViewModels/Homepage.cs
@@ -13,9 +13,22 @@ namespace puckweb.ViewModels
     public class Homepage:Page
     {
         [Display(Name="Carousel Items",GroupName ="Content")]
-        [PuckPickerEditorSettings(MaxPick =5)]
+        //this is a content picker, the settings attribute specifies maximum number of selections and the types of content you're allowed to select
+        [PuckPickerEditorSettings(MaxPick =5,Types =new Type[] {typeof(Homepage),typeof(Section),typeof(Page)})]
         [UIHint(EditorTemplates.PuckPicker)]
-        
         public List<PuckPicker> CarouselItems { get; set; }
+
+        [Display(GroupName = "Selects")]
+        [UIHint(EditorTemplates.SelectList)]
+        //select item Label and Value are separated with ":" by default, you can change this using the Separator property of the SelectListSettings attribute below
+        //if the Label and Value are the same, you can simply specify the Label only
+        [SelectListSettings(Values = new string[] { "London:UK", "Paris", "Tokyo:Japan" })]
+        public string City { get; set; }
+
+        [Display(GroupName = "Selects")]
+        [UIHint(EditorTemplates.MultiSelectList)]
+        [SelectListSettings(Values = new string[] { "London:UK", "Paris", "Tokyo:Japan" })]
+        public List<string> Cities { get; set; }
+
     }
 }
